474c19c57dced541dc109c72202c2bbb880a7b08
Tim <tim.z@mofadeng.com>
2018-12-01 12:20:01 +1300

    Add Todo Item

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { API, graphqlOperation } from 'aws-amplify';
-import {listTodos} from "./graphql/queries";
+import * as queries from "./graphql/queries";
+import * as mutations from "./graphql/mutations";
+import * as subscriptions from "./graphql/subscriptions";
 
 class App extends Component {
-  state = { todos: [] }
+  state = {
+    todo: '',
+    todos: [],
+  };
   async componentDidMount() {
-    const data = await API.graphql(graphqlOperation(listTodos))
+    const data = await API.graphql(graphqlOperation(queries.listTodos))
     this.setState({
       todos: data.data.listTodos.items
     })
   }
+  addTodo = async() => {
+    if (this.state.todo === '' ) return;
+    const todo = {
+      title: this.state.todo,
+      completed: false,
+    };
+    const data = await API.graphql(graphqlOperation(mutations.createTodo, {input:todo}));
+    console.log(data);
+    this.setState({ todo: '' })
+  };
   render() {
     return (
       <div className="App">
+        <input
+          onChange={e => this.setState({ todo: e.target.value })}
+          value={this.state.todo}
+          placeholder='Todo name'
+        />
+        <button onClick={this.addTodo}>Add Todo</button>
         {
           this.state.todos.map((todo) => (
             <p key={todo.id}>
